aad1c44c5cdd1fab4301b1b96bbc2c31c21d4060
Bhavesh P <bpwankhede@cgsinc.com>
2026-01-21 19:39:12 +0530

    Added badges

--- prep-ai/src/app/pages/feedback/feedback.page.ts
+++ prep-ai/src/app/pages/feedback/feedback.page.ts
@@ -17,7 +17,8 @@ export class FeedbackPage implements OnInit {
   loading = true;
   totalScore = 0;
   averageScore = 0;
-
+bestIndex = -1;
+weakestIndex = -1;
   constructor(private ai: AiService) {}
 
   ngOnInit() {
@@ -25,6 +26,8 @@ export class FeedbackPage implements OnInit {
   }
 
   ionViewWillEnter() {
+    this.bestIndex = -1;
+this.weakestIndex = -1;
     this.results = [];
   this.averageScore = 0;
 
@@ -94,7 +97,33 @@ export class FeedbackPage implements OnInit {
 
   finish() {
     this.averageScore = Math.round(this.totalScore / this.results.length);
+  
     this.loading = false;
+  // 🔥 FIND BEST & WEAKEST QUESTION
+// 🔥 FIND BEST & WEAKEST ONLY IF MORE THAN 1 QUESTION
+if (this.results.length > 1) {
+
+  let maxScore = -1;
+  let minScore = 11;
+
+  this.results.forEach((r, i) => {
+    if (r.score > maxScore) {
+      maxScore = r.score;
+      this.bestIndex = i;
+    }
+
+    if (r.score < minScore) {
+      minScore = r.score;
+      this.weakestIndex = i;
+    }
+  });
+
+} else {
+  // Only one question → no best / weakest
+  this.bestIndex = -1;
+  this.weakestIndex = -1;
+}
+
 
     const history = JSON.parse(localStorage.getItem('history') || '[]');
     history.push({
